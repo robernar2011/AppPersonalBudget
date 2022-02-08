42e34265f87640c4fdf2e88f558aeffb5558c86c
robernar2011 <robernar2011@gmail.com>
2022-02-08 14:37:34 -0300

    Listing expenses (Part 2)

--- app.js
+++ app.js
@@ -114,6 +114,33 @@ function carregaListaDespesas(){
 
 	despesas = bd.recuperarTodosRegistros()
 
-	console.log(despesas)
+	let listaDespesas = document.getElementById('listaDespesas')
+
+	//percorrer o array despesas, listanda cada despesa de forma dinâmica
+	despesas.forEach(function(d){
+
+		//criar linha(tr)
+		var linha = listaDespesas.insertRow()
+
+		//criar colunas(td)
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+		
+		switch(d.tipo){
+			case '1': d.tipo = 'Alimentação'
+				break
+			case '2': d.tipo = 'Educação'
+				break
+			case '3': d.tipo = 'Lazer'
+				break
+			case '4': d.tipo = 'Saúde'
+				break
+			case '5': d.tipo = 'Transporte'
+				break
+		}
+		linha.insertCell(1).innerHTML = d.tipo
+
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+	})
 }
 
