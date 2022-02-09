9815e11b5d4626733848ee40a290e01e68a1417b
robernar2011 <robernar2011@gmail.com>
2022-02-09 14:30:27 -0300

    Function Remove

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ class Bd{
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 		}
 		return despesas
@@ -100,6 +101,10 @@ class Bd{
 		}
 		
 		return despesasFiltradas
+	}
+
+	remover(id){
+		localStorage.removeItem(id)
 	}
 }
 
@@ -190,6 +195,21 @@ function carregaListaDespesas(despesas = [], filtro = false){
 
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		//Botão de Exclusão
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function(){
+			//remover a despesa
+			let id = this.id.replace('id_despesa_', '')
+
+			bd.remover(id)
+
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
 	})
 }
 
